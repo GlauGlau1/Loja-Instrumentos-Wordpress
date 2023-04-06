5cc847d5c6bebf77f28ed15bee2a7a0cc7dd8216
GlauGlau1 <glauberjjose16@gmail.com>
2023-04-06 20:31:50 -0300

    separando

--- index.php
+++ index.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
-</head>
-<body>
+<?php get_header(); ?>
     <h1>BOM VINDO</h1>
-</body>
-</html>
+<?php get_footer() ?>
